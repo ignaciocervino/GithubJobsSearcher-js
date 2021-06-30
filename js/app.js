f6b94fbc5d2e6e94f9916968c4c9814773d0d7da
ignaciocervino <ignaciocervino@gmail.com>
2021-06-30 12:42:06 -0300

    #06 Connecting to API

--- js/app.js
+++ js/app.js
@@ -11,14 +11,17 @@ function validarBusqueda(e){
 
     if (busqueda.length<3) {
         mostrarMensaje('Busqueda muy corta... Añade mas informacion');
+        return;
     }
+
+    consultarAPI(busqueda);
 }
 
 function mostrarMensaje(mensaje){
     const alertaPrevia = document.querySelector('.alerta');
     if (!alertaPrevia) {
         const alerta = document.createElement('div');
-        alerta.classList.add('bh-gray-100','p-3', 'text-center','mt-3','alerta');
+        alerta.classList.add('bg-gray-100','p-3', 'text-center','mt-3','alerta');
         alerta.textContent=mensaje;
 
         formulario.appendChild(alerta);
@@ -28,4 +31,35 @@ function mostrarMensaje(mensaje){
         }, 3000);
     }
     
+}
+
+function consultarAPI(busqueda){
+    const githubURL=`https://jobs.github.com/positions.json?search=${busqueda}`;
+    const url = `https://api.allorigings.win/get?url=${encodeURIComponent(githubURL)}`;
+
+    axios.get(url)
+        .then(respuesta => mostrarVacantes(JSON.parse(respuesta.data.contents)))
+}
+
+function mostrarVacantes(vacantes){
+    while (resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
+
+    if (vacantes.length>0) {
+        resultado.classList.add('grid');
+
+        vacantes.forEach(vacante => {
+            const {company,title,type,url} = vacante;
+
+            resultado.innerHTML +=`
+                <div class="shadow bg-white p-6 rounded">
+                    <h2 class="text-2xl font-light mb-4">${title}</h2>
+                    <p class="font-bold uppercase">Compañia:  <span class="font-light normal-case">${company} </span></p>
+                    <p class="font-bold uppercase">Tipo de Contrato:   <span class="font-light normal-case">${type} </span></p>
+                    <a class="bg-teal-500 max-w-lg mx-auto mt-3 rounded p-2 block uppercase font-xl font-bold text-white text-center" href="${url}">Ver Vacante</a>
+                </div>
+            `;
+        });
+    }
 }
